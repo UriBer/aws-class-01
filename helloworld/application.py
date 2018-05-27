7e7c277e86121c645d37db5de5abdb635b09a8bd
EC2 Default User <ec2-user@ip-172-31-87-238.ec2.internal>
2018-05-27 06:08:46 +0000

    after adding session

--- helloworld/application.py
+++ helloworld/application.py
@@ -7,6 +7,9 @@ from flask import Flask, Response, render_template, request
 from helloworld.flaskrun import flaskrun
 import requests
 
+import boto3
+import datetime
+
 application = Flask(__name__)
 
 @application.route('/', methods=['GET'])
@@ -17,8 +20,37 @@ def get():
 @application.route('/get_ip', methods=['GET'])
 def get_ip():
     # print(get_ip_meta())
+    # return time and path to url to database
     return Response(json.dumps(get_ip_meta()), mimetype='application/json', status=200)
 
+@application.route('/temp/<temp>', methods=['GET'])
+def get_temp(temp):
+
+    response = get_ip_meta()
+    my_ses = boto3.Session()
+    dynamodb = my_ses.resource('dynamodb')
+    table = dynamodb.Table('eb_try_logger')
+
+    item={
+    'ip_addr': str(response), 
+    'path': temp,
+    'datetime': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+    'time': datetime.datetime.now().strftime("%H:%M:%S"),
+    'ip_meta' : response, # res_data
+    'name':'chilkibilki'
+    }
+    print(item)
+    table.put_item(Item=item)
+    return Response(json.dumps(item), mimetype='application/json', status=200)
+
+@application.route('/bi', methods=['GET'])
+def get_bi():
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('eb_try_logger')
+    resp = table.scan()
+
+    #return Response(json.dumps(str(resp)), mimetype='application/json', status=200)
+    return render_template('index.html', response=str(resp), title='bi')
 
 @application.route('/new/<param>', methods=['GET'])
 def get_param(param):
@@ -39,7 +71,7 @@ def options():
 def get_ip_meta():
     user_ip = str(request.environ['REMOTE_ADDR'])
     service_url = 'http://ipinfo.io/{}'.format(user_ip) 
-
+    
     return requests.get(service_url).json()
 
 
